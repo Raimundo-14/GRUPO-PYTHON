3f194a63b006befae5fcd09b61a9d22de3d0224d
Raimundo Albuquerque <102672431+Raimundo-14@users.noreply.github.com>
2023-10-30 21:38:50 -0300

    lista com for

---
+++ ex-lista-for-01.py
@@ -0,0 +1,6 @@
+magicos = ['alice','fred', 'david', 'carolina']
+for magico in magicos:
+    print(magico.title() + ', que grande truque!!')
+    print('Mal posso esperar para ver seu proximo truque, ' + magico.title() + '.\n')
+    print('Obrigado, a todos. Foi um grande show de mágica!')
+    
