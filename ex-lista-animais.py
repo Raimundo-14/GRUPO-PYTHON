ad367469ef208483dd68635b464dff745c478246
Raimundo Albuquerque <102672431+Raimundo-14@users.noreply.github.com>
2023-10-30 22:23:51 -0300

    lista for-identação

---
+++ ex-lista-animais.py
@@ -0,0 +1,3 @@
+cachorros = ['doberman', 'pit-bull', 'cane-corso']
+for cachorro in cachorros:
+    print(cachorro.title() + ', seria um bom cão de guarda!!')
---
+++ ex-lista-for-pizza.py
@@ -0,0 +1,5 @@
+pizzas = ['marguerita', 'lombinho', 'dois amores']
+for pizza in pizzas:
+    print(pizza.title())
+    print('Eu adoro pizza de, ' + pizza.title())
+    print('Eu realmente, adoro pizza')
