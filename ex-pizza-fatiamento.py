583e25705cdf0c9b22717ff8a5529879772f343d
Raimundo Albuquerque <102672431+Raimundo-14@users.noreply.github.com>
2023-11-02 22:33:33 -0300

    exemplos de tuplas

--- ex-pizza-fatiamento.py
+++ ex-pizza-fatiamento.py
@@ -4,7 +4,6 @@ minhas_pizzas.append('peperoni')
 sua_pizza.append('4 queijos')
 print('Minha lista de pizzas favoritas são estas:')
 for minhas_pizza in minhas_pizzas[:]:
-
     print(minhas_pizza.title())
 print(minhas_pizzas)
 print(sua_pizza)
